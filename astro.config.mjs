c563284299c141f1ddf1f801e3e66a490d8314b5
Jesus Diaz <jadiaz.inf@gmail.com>
2025-09-29 13:28:44 -0400

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,22 @@ import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
   integrations: [svelte()],
+
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
     routing: { prefixDefaultLocale: true },
   },
+
+  adapter: vercel(),
 });
