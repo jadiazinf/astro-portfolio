af83d4cb06907c39fc684aa52b1a494ed0a9b9c5
Jesus Diaz <jadiaz.inf@gmail.com>
2025-09-30 12:02:30 -0400

    fix: delete .vercel folder

--- .vercel/output/_functions/chunks/types_mIFp1dTE.mjs
+++
@@ -1,8 +0,0 @@
-var ETheme = /* @__PURE__ */ ((ETheme2) => {
-  ETheme2["LIGHT"] = "light";
-  ETheme2["DARK"] = "dark";
-  return ETheme2;
-})(ETheme || {});
-const THEME_NAME = "theme";
-
-export { ETheme as E, THEME_NAME as T };
--- .vercel/output/_functions/pages/index.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../chunks/index_SVTskzfH.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_DY5zhuKj.mjs';
-
-const page = () => _page;
-
-export { page };
